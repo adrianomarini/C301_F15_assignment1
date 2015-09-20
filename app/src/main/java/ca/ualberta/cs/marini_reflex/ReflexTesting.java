0e39aa4ef4be08ccc6bfc6d6bae0d0ee2a436bc6
Adriano Marini <marini@ualberta.ca>
2015-09-20 15:42:44 -0600

    dded 2 new activities for the main menu things.
    Future additions:
    - 2 player mode
    - 3 player mode
    - 4 player mode
    - Result activity
    
    - and said implelemtations
    `

--- app/src/main/java/ca/ualberta/cs/marini_reflex/ReflexTesting.java
+++ app/src/main/java/ca/ualberta/cs/marini_reflex/ReflexTesting.java
@@ -5,18 +5,18 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainMenu extends AppCompatActivity {
+public class ReflexTesting extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
+        setContentView(R.layout.activity_reflex_testing);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_menu, menu);
+        getMenuInflater().inflate(R.menu.menu_reflex_testing, menu);
         return true;
     }
 
